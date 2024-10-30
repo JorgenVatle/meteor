8aa2f3850df70037b0dc7719b9e7e12ab4bcd1ff
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-30 17:33:49 +0200

    Add esbuild plugin for rewriting meteor imports

--- isopack-v2/tsup.config.ts
+++ isopack-v2/tsup.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'tsup';
+import Path from 'node:path';
 
 export default defineConfig({
-    entry: ['./src/main.ts'],
+    entry: ['./src/main.ts', 'test-app/app.js'],
     skipNodeModulesBundle: true,
     sourcemap: true,
+    noExternal: ['meteor'],
+    esbuildPlugins: [
+        {
+            name: 'meteor',
+            setup(build) {
+                build.onResolve({ filter: /^meteor\// }, (args) => {
+                    const [meteor, name] = args.path.split('/');
+                    const result = {
+                        path: Path.join(process.cwd(), '_packageDist', name, 'server'),
+                        external: true,
+                    }
+                    
+                    console.log({ meteor, name, result });
+                    return result;
+                });
+            }
+        }
+    ]
 })
