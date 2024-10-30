125bc010fbd348553058bdbaf2a4f80853ebcb31
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-30 21:49:46 +0200

    Add warn and error loggers

--- isopack-v2/src/lib/Logger.ts
+++ isopack-v2/src/lib/Logger.ts
@@ -3,6 +3,8 @@ import { DEBUG } from '../Config';
 export const Logger = {
     log: console.log,
     info: console.info,
+    error: console.error,
+    warn: console.warn,
     debug(...args: [message: any, ...params: any]) {
         if (!DEBUG) {
             return;
