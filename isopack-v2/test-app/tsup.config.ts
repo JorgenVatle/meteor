122f13ba3522d78c4fafe429f7293a09ad61d695
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-30 21:19:01 +0200

    Remove unused tsup config

--- isopack-v2/test-app/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-import tsupConfig from '../tsup.config';
-
-export default defineConfig({
-    ...tsupConfig,
-    outDir: '../dist/test-app',
-    entry: ['./app.ts'],
-    tsconfig: '../tsconfig.json',
-});
