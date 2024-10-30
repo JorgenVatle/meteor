af1de00710efff1d753e53f80d0505e82b8a5482
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-30 17:47:35 +0200

    Log startup

--- isopack-v2/test-app/app.ts
+++ isopack-v2/test-app/app.ts
@@ -1,3 +1,8 @@
-import 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
 import 'meteor/webapp';
 import 'meteor/ddp';
+
+console.log('Starting Meteor...');
+Meteor.startup(() => {
+    console.log('Meteor started successfully')
+})
