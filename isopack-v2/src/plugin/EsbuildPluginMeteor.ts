932d688409e49cb02663f1caa4f4ddd04a7bf91d
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-30 23:31:03 +0200

    Update server entry path

--- isopack-v2/src/plugin/EsbuildPluginMeteor.ts
+++ isopack-v2/src/plugin/EsbuildPluginMeteor.ts
@@ -14,7 +14,7 @@ export function meteor({ external = true } = {}): Plugin {
             build.onResolve({ filter: /^meteor\// }, (args) => {
                 const [meteor, name] = args.path.split('/');
                 const result = {
-                    path: Path.join(PACKAGE_DIST_DIR, name, 'server.js'),
+                    path: Path.join(PACKAGE_DIST_DIR, name, 'server.mjs'),
                     external,
                 }
                 
