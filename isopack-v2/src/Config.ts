f2bf151980923d294af54295ce7a09494028e1d7
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-30 21:15:45 +0200

    Revert back to using .packages directory for builds

--- isopack-v2/src/Config.ts
+++ isopack-v2/src/Config.ts
@@ -1,7 +1,7 @@
 import Path from 'node:path';
 import process from 'node:process';
 
-export const PACKAGE_TEMP_DIR = Path.join(process.cwd(), 'dist', 'packages');
+export const PACKAGE_TEMP_DIR = Path.join(process.cwd(), '.packages');
 export const BUNDLE_ASSETS_DIR = Path.join(process.cwd(), 'src', 'BundleAssets');
 export const PACKAGE_DIST_DIR = Path.join(PACKAGE_TEMP_DIR, 'dist');
 export const PACKAGE_ENTRY_DIR = Path.join(PACKAGE_TEMP_DIR, 'entry');
