1cb76ce97507fce74832d4d06f17177b8dfbdd35
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-30 21:17:30 +0200

    Build both test app and source with main tsup config

--- isopack-v2/tsup.config.ts
+++ isopack-v2/tsup.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'tsup';
 import { meteor } from './src/plugin/MeteorImports';
 
 export default defineConfig({
-    entry: ['./src/main.ts'],
-    outDir: './dist/src',
+    entry: [
+        './src/main.ts',
+        './test-app/app.ts',
+    ],
+    outDir: './dist',
     skipNodeModulesBundle: true,
     clean: true,
     sourcemap: true,
