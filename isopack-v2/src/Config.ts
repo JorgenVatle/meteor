6f85731ac6936a224d186407e57bfb4dd632a085
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-31 01:27:23 +0200

    Use consistent dirname for paths

--- isopack-v2/src/Config.ts
+++ isopack-v2/src/Config.ts
@@ -1,10 +1,11 @@
 import Path from 'node:path';
 import process from 'node:process';
 
-export const PACKAGE_TEMP_DIR = Path.join(process.cwd(), '.packages');
-export const BUNDLE_ASSETS_DIR = Path.join(process.cwd(), 'src', 'BundleAssets');
+export const ROOT_DIR = Path.join(__dirname, '..', '..');
+export const PACKAGE_TEMP_DIR = Path.join(ROOT_DIR, '.packages');
+export const BUNDLE_ASSETS_DIR = Path.join(ROOT_DIR, 'src', 'BundleAssets');
 export const PACKAGE_DIST_DIR = Path.join(PACKAGE_TEMP_DIR, 'dist');
 export const PACKAGE_ENTRY_DIR = Path.join(PACKAGE_TEMP_DIR, 'entry');
 export const TYPES_DIST_DIR = Path.join(PACKAGE_TEMP_DIR, 'types', 'meteor');
-export const PACKAGE_TSCONFIG_FILE = Path.join(process.cwd(), 'tsconfig.packages.json');
+export const PACKAGE_TSCONFIG_FILE = Path.join(ROOT_DIR, 'tsconfig.packages.json');
 export const DEBUG = !!process.env.DEBUG;
