e92f747f2c981e93f5df44180eea4c24dab91ba9
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-31 03:51:23 +0200

    Add package source dir

--- isopack-v2/src/Config.ts
+++ isopack-v2/src/Config.ts
@@ -5,6 +5,7 @@ import process from 'node:process';
 export const ROOT_DIR = Path.join(process.env.PWD || '');
 
 export const PACKAGE_TEMP_DIR = Path.join(ROOT_DIR, 'dist', 'packages');
+export const PACKAGE_SRC_DIR = Path.join(ROOT_DIR, '..', 'packages');
 export const PACKAGE_DIST_DIR = Path.join(PACKAGE_TEMP_DIR, 'dist');
 export const PACKAGE_ENTRY_DIR = Path.join(PACKAGE_TEMP_DIR, 'entry');
 export const PACKAGE_TSCONFIG_FILE = Path.join(ROOT_DIR, 'tsconfig.packages.json');
