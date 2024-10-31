41c8f7d239326044a48c2762ddfbddeec2ee5918
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-31 03:25:40 +0200

    Add helper function for creating import strings

--- isopack-v2/src/lib/Helpers.ts
+++ isopack-v2/src/lib/Helpers.ts
@@ -11,3 +11,41 @@ export function normalizeOptionalArray<TType extends string>(input: TType | TTyp
 export function packagePath(name: string) {
     return Path.join(process.cwd(), '..', 'packages', name, 'package.js');
 }
+
+export function esmImportString(config: {
+    /**
+     * Path to the module to import. Can be absolute or relative.
+     */
+    path: string;
+    /**
+     * Whether to create a relative import path, starting from the directory of this file.
+     */
+    fromFile?: string;
+    
+    /**
+     * Same as fromFile, just starting from the provided directory.
+     */
+    fromDir?: string;
+    
+    /**
+     * Whether to re-export everything exported by the provided module.
+     */
+    reExport?: boolean;
+}) {
+    let from: string | undefined = config.fromDir;
+    let path = config.path;
+    
+    if (config.fromFile) {
+        from = Path.dirname(config.fromFile);
+    }
+    
+    if (from) {
+        path = Path.relative(from, path);
+    }
+    
+    if (config.reExport) {
+        return `export * from ${JSON.stringify(path)}`;
+    }
+    
+    return `import ${JSON.stringify(path)}`;
+}
