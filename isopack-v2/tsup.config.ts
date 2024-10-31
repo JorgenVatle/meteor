f2faf6285447ec9bb3e51f378a8d00fe464d1c1c
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-31 02:28:32 +0200

    Set target platform and disable splitting

--- isopack-v2/tsup.config.ts
+++ isopack-v2/tsup.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     sourcemap: true,
     cjsInterop: true,
     shims: true,
+    splitting: false,
+    platform: 'node',
+    target: 'node20',
     noExternal: ['meteor'],
     format: 'esm',
     esbuildPlugins: [
