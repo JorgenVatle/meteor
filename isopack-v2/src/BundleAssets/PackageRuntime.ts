777fe90d2f95240185b2f1988de10a6578ec8cb3
Jørgen Vatle <jorgen@wcaservices.net>
2024-10-31 04:26:39 +0200

    Don't export Npm class

--- isopack-v2/src/BundleAssets/PackageRuntime.ts
+++ isopack-v2/src/BundleAssets/PackageRuntime.ts
@@ -1,7 +1,7 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-export class Npm {
+class Npm {
     public require(name: string) {
         return require(name);
     }
