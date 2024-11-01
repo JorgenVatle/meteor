0223f21573517c73333949a1e0e28b1e7df01bb7
Jørgen Vatle <jorgen@wcaservices.net>
2024-11-01 13:46:04 +0200

    Log contents of meteor module

--- isopack-v2/test-app/app.ts
+++ isopack-v2/test-app/app.ts
@@ -1,6 +1,9 @@
 import { Meteor } from 'meteor/meteor';
+import * as MeteorModule from 'meteor/meteor';
 import 'meteor/webapp';
 import 'meteor/ddp';
+
+console.dir(MeteorModule, { colors: true, depth: 3, getters: 'get' });
 
 console.log('Starting Meteor...');
 Meteor.startup(() => {
