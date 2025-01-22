43debc1493287c2afa66944ec21c329a7c70bf3c
Jørgen Vatle <Jorgen@WcAServices.net>
2025-01-22 21:32:04 +0200

    Configure Meteor-Vite using plugin rather than deprecated top-level property.

--- tools/static-assets/skel-vue/vite.config.js
+++ tools/static-assets/skel-vue/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
+import { meteor } from 'meteor-vite/plugin';
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  meteor: {
-    clientEntry: 'imports/ui/main.js',
-  },
+  plugins: [
+    vue(),
+    meteor({
+      clientEntry: 'imports/ui/main.js'
+    })
+  ],
   optimizeDeps: {
     exclude: ['vue-meteor-tracker'],
   },
